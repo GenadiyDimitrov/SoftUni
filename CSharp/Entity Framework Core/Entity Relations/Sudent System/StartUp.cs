6faa07d7702f57b17704d1106a9f587312e0c471
Genadiy Dimitrov <genadiidimitrov@gmail.com>
2023-11-25 13:51:46 +0200

    Change Solution tree Added solution for Student System

---
+++ CSharp/Entity Framework Core/Entity Relations/Sudent System/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    internal class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new StudentSystemContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
